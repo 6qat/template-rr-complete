d5d44ef17cd29f512beb22ded2284beae6314432
Guilherme C. B. Moreira <ceschiatti@gmail.com>
2025-03-12 15:13:32 -0300

    Playing with component arguments, loaders and actions

--- app/routes/lab1.tsx
+++ app/routes/lab1.tsx
@@ -1,11 +1,44 @@
 import { Outlet } from 'react-router';
+import type { Route } from './+types/lab1';
 
 // https://reactrouter.com/how-to/file-route-conventions
 
-export default function Lab1() {
+export async function loader({ params }: Route.LoaderArgs) {
+  console.log(params);
+  return { message: 'Hello, world!' };
+}
+
+export async function clientLoader({ serverLoader }: Route.ClientLoaderArgs) {
+  const _serverData = await serverLoader();
+  const data = {};
+  return data;
+}
+
+export async function action({ params }: Route.ActionArgs) {
+  console.log(params);
+  return { message: 'Hello, world!' };
+}
+
+export async function clientAction({ serverAction }: Route.ClientActionArgs) {
+  const _serverData = await serverAction();
+  const data = {};
+  return data;
+}
+
+export default function Lab1({
+  loaderData,
+  actionData,
+  params,
+  matches,
+}: Route.ComponentProps) {
   return (
     <>
       <div>Lab1</div>
+      <h1>Welcome to My Route with Props!</h1>
+      <p>Loader Data: {JSON.stringify(loaderData)}</p>
+      <p>Action Data: {JSON.stringify(actionData)}</p>
+      <p>Route Parameters: {JSON.stringify(params)}</p>
+      <p>Matched Routes: {JSON.stringify(matches)}</p>
       <Outlet />
     </>
   );
