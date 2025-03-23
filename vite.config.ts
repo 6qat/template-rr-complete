daf040353a7d439395e74f9688583c8688a6ecc7
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2025-03-22 21:56:25 -0300

    Corret vit config for bun

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,12 @@ import tailwind from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [reactRouter(), tsconfigPaths(), tailwind()],
+  resolve:
+    process.env.NODE_ENV === 'development'
+      ? {}
+      : {
+          alias: {
+            'react-dom/server': 'react-dom/server.node',
+          },
+        },
 });
